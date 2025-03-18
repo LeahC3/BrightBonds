e6b602314f27425a6e4a5db7fe1799eb253db6b9
Leah <109706716+LeahC3@users.noreply.github.com>
2025-03-17 21:00:32 -0400

    Optimized Navbar for Mobile

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Toggle between showing and hiding page menu when hamburger icon clicked*/
+function togglePages() {
+    const pages = document.getElementsByClassName("page");
+    
+    if (pages[0].style.fontSize !== "1.1rem") {
+        for (let i = 0; i < pages.length; i++)
+            pages[i].style.padding = "0.6rem 1.5rem";
+        
+        for (let i = 0; i < pages.length; i++)
+            pages[i].style.fontSize = "1.1rem";
+    } else {
+        for (let i = 0; i < pages.length; i++)
+            pages[i].style.padding = "0rem";
+        
+        for (let i = 0; i < pages.length; i++)
+            pages[i].style.fontSize = "0rem";
+    }
+
+  }
